ccac1a3e36a8b5975ec57ef61fcf5e845c7875a8
Felix <felixfalkovsky@gmail.com>
2023-06-16 12:27:12 +0300

    initial commit

---
+++ AppStoreAsync/App/AppStoreAsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreAsyncApp.swift
+//  AppStoreAsync
+//
+//  Created by Felix Falkovsky on 16.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreAsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
